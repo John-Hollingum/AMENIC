3c076132b3afb1ab3232c21784c3d7f2d3276b7f
johnhollingum <j.s.hollingum@gmail.com>
2022-02-04 17:28:08 +0000

    Clean out demo files. Refine UI

--- amenic.py
+++ amenic.py
@@ -1,18 +1,25 @@
+import jsonpickle # pip install jsonpickle
+import json
+from decimal import *
+from functools import partial
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QPixmap, QPalette, QIcon
+from PyQt5.QtGui import QPixmap, QPalette, QIcon, QColor, QStandardItemModel, QStandardItem
 import sys
 import vlc
 
 voices = []
 
 class TableModel(QAbstractTableModel):
+
+	header_labels = ['N No.', 'N Name', ' ', 'Image File']
+
 	def __init__(self, data):
 		super().__init__()
 		self._data = data
 
 	def flags(self, index):
-		if index.column() == 0:
+		if index.column() == 0 or index.column == 2:
 			# col 0 is editable
 			return Qt.ItemIsSelectable|Qt.ItemIsEnabled|Qt.ItemIsEditable
 		else:
@@ -25,10 +32,21 @@ class TableModel(QAbstractTableModel):
 			return True
 		return False
 
+	def headerData(self, section, orientation, role=Qt.DisplayRole):
+		if role == Qt.DisplayRole and orientation == Qt.Horizontal:
+			return self.header_labels[section]
+		return QAbstractTableModel.headerData(self, section, orientation, role)
+
 	def data(self, index, role):
 		if index.isValid():
+			if role == Qt.BackgroundRole:
+				if index.column() == 1 or index.column() == 3:
+					return QColor('#eeeeee')
+
 			if role == Qt.DisplayRole or role == Qt.EditRole:
 				val = self._data[index.row()][index.column()]
+				if index.column() == 0:
+					self._data[index.row()][1] = to_name(int(Decimal(val)))
 				return str(val)
 
 	def rowCount(self, index):
@@ -40,51 +58,98 @@ class TableModel(QAbstractTableModel):
 		# the length (only works if all rows are an equal length)
 		return len(self._data[0])
 
+
 class voice():
 
 	def __init__(self):
-		self.defImg = None
-		self.restImg = None
-		self.imgTable = [ [ None,None]]
-		self.name = 'Untitled'
+		self.vdata = {
+			"defImg": None,
+			"restImg": None,
+			"imgTable": [ [ 0,'']],
+			"name": 'Untitled'
+		}
+
+	def edit(self):
 		ve = vEditD(self)
 		ve.exec_()
+		rval = ve.getVal()
+		# the brevity of this next line is the entire point of using the vdata dict
+		# to carry the data in voice:
+		self.vdata = rval.vdata.copy()
+		return
 
 class vEditD(QDialog):
 	def __init__(self, v:voice):
 		super().__init__()
+		self.setModal(True)
 		self.initUI(v)
+
+	def getImg(self,idx):
+		( fname, filter)  = QFileDialog.getOpenFileName(self, 'Open file', '~/Documents',"Image files (*.png, *.jpg)")
+		self.model._data[idx][3] = fname
+
+	def getDefImg(self,idx):
+		( fname, filter)  = QFileDialog.getOpenFileName(self, 'Open Default Image', '~/Documents',"Image files (*.png, *.jpg)")
+		self.diEdit.setText(fname)
+
+	def getRestImg(self,idx):
+		( fname, filter)  = QFileDialog.getOpenFileName(self, 'Open Rest Image', '~/Documents',"Image files (*.png, *.jpg)")
+		self.restEdit.setText(fname)
 
 	def initUI(self, v:voice):
 		self.setWindowTitle('Edit Voice')
 		self.resize(400,600)
+
 		# set up widgets
 		self.nameLabel = QLabel('Name')
 		self.nameEdit = QLineEdit(self)
-		self.nameEdit.setText(v.name)
+		self.nameEdit.setText(v.vdata["name"])
 		self.nameEdit.textChanged.connect(self.maybeActiveSave)
 
 		self.diLabel = QLabel("Default Image")
 		self.diEdit = QLineEdit(self)
-		self.diEdit.setText(v.defImg)
+		self.diEdit.setText(v.vdata.get("defImg"))
+		self.diEdit.setToolTip("Image shown when any key pressed unless overridded in note map table")
 		self.diLookup = QPushButton('>')
+		self.diLookup.clicked.connect(self.getDefImg)
 
 		self.restLabel = QLabel("Rest Image")
 		self.restEdit = QLineEdit(self)
-		self.restEdit.setText(v.restImg)
+		self.restEdit.setText(v.vdata["restImg"])
+		self.restEdit.setToolTip("Image, in monophonic mode shown when no note playing")
 		self.restLookup = QPushButton('>')
+		self.restLookup.clicked.connect(self.getRestImg)
 
 		self.noteMap = QTableView()
-		self.model = TableModel(v.imgTable)
+		#self.model = TableModel()
+		# this row insert routine probably wants extracting to another function
+		# while the actual data in the model comes from vdata["imgTable"],
+		# there's a lot of 'presentation' stuff in the model that we don't want
+		# to store. Conversely, we probably want to retain the bitmap, or a ref
+		# to it in vdata["imgTable"] which wouldn't be presented.
+		presTable = []
+		for idx in range(0, len(v.vdata["imgTable"])):
+			presTable.append([ v.vdata["imgTable"][idx][0], to_name(v.vdata["imgTable"][idx][0]), None, v.vdata["imgTable"][idx][1] ])
+
+		self.model = TableModel(presTable)
 		self.noteMap.setModel(self.model)
-		#self.noteMap.horizontalHeaderItem
+		for idx in range(0, len(v.vdata["imgTable"])):
+			btnGetImg = QPushButton("Select image")
+			btnGetImg.clicked.connect(partial(self.getImg, idx))
+			self.noteMap.setIndexWidget(self.model.index(idx, 2), btnGetImg)
+
+		self.noteMap.setColumnWidth(0,60)
+		self.noteMap.setColumnWidth(1,60)
+		self.noteMap.setColumnWidth(3,180)
 
 		self.btnCancel = QPushButton('Cancel')
 		self.btnCancel.clicked.connect(self.cancelOut)
 
 		self.btnSave = QPushButton('Save')
+		self.btnSave.clicked.connect(self.saveOut)
 		self.btnSave.setEnabled(False)
 
+		# Layout
 		vbox = QVBoxLayout()
 		hbox1=QHBoxLayout()
 		hbox1.addWidget(self.nameLabel)
@@ -111,8 +176,23 @@ class vEditD(QDialog):
 		self.setLayout(vbox)
 
 	def cancelOut(self):
+		self.saveV = voice()
 		self.reject()
 		return None
+
+	def saveOut(self,v:voice):
+		self.saveV = voice()
+		self.saveV.vdata["name"] = self.nameEdit.text()
+		self.saveV.vdata["defImg"] = self.diEdit.text()
+		self.saveV.vdata["restImg"] = self.restEdit.text()
+		# self.saveV.imgTable -- what exactly?
+		for idx in range(0, len(self.model._data)):
+			self.saveV.vdata["imgTable"].append([ self.model._data[idx][0], self.model._data[idx][3]])
+		self.accept()
+
+	def getVal(self):
+		return self.saveV
+		#mess("I think I'm done here")
 
 	def maybeActiveSave(self):
 		s = self.nameEdit.text
@@ -124,7 +204,9 @@ class vEditD(QDialog):
 class QAmenicMain(QDialog) :
 	def __init__(self):
 		super().__init__()
-		self.setWindowIcon(QIcon('AmenicIcon.png'))
+		icon = QIcon()
+		icon.addPixmap(QPixmap("AmenicIcon.png"), QIcon.Selected, QIcon.On)
+		self.setWindowIcon(icon)
 		self.initUI()
 		self.vtable = []
 
@@ -197,7 +279,10 @@ class QAmenicMain(QDialog) :
 				i = self.dvCombo.findText("<none>")
 				self.dvCombo.setCurrentIndex(i)
 			else:
+				self.droneCombo.addItem(self.playerVn)
 				self.dvCombo.addItem(self.playerVn)
+				i = self.dvCombo.findText(self.playerVn)
+				self.dvCombo.setCurrentIndex(i)
 			return
 		elif self.dvCombo.currentText() != "<none>":
 			mess("use "+self.dvCombo.currentText())
@@ -212,6 +297,9 @@ class QAmenicMain(QDialog) :
 				self.droneCombo.setCurrentIndex(i)
 			else:
 				self.droneCombo.addItem(self.droneVn)
+				self.dvCombo.addItem(self.droneVn)
+				i = self.droneCombo.findText(self.droneVn)
+				self.droneCombo.setCurrentIndex(i)
 				# find droneV from droneVn
 
 		elif self.droneCombo.currentText() != "<none>":
@@ -225,12 +313,13 @@ class QAmenicMain(QDialog) :
 		# need to be vigilant about confusing the index of voices with
 		# the index of the combobox
 		self.activeV=voice()
-		if self.activeV.name != "Untitled" and self.activeV.name != '' and self.activeV.name != None:
+		self.activeV.edit()
+		vn = self.activeV.vdata["name"]
+		if vn != "Untitled" and vn != '' and vn != None:
 			voices.append(self.activeV)
-			return self.activeV.name
+			return vn
 		else:
 			return None
-
 
 	def getSound(self):
 		( fname, filter)  = QFileDialog.getOpenFileName(self, 'Open file', '~/Documents',"Sound files (*.mp3)")
@@ -245,6 +334,14 @@ class QAmenicMain(QDialog) :
 	def stopsound(self):
 		self.st.stop()
 		self.btnStop.setEnabled(False)
+
+
+def to_name(nn):
+	nnt = [ "C","C#","D","D#","E","F","F#","G","G#","A", "A#","B"]
+	on = int(nn / 12) -1 # opinion seems to differ about whether it's necessary to subtract 1
+	nis = nn % 12
+	nname = nnt[nis]
+	return nname + " " + str(on)
 
 def mess(mymess):
 	msg = QMessageBox()
