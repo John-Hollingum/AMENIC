1ff4bed2692533c499d6848481deb87f024f3e81
johnhollingum <j.s.hollingum@gmail.com>
2022-02-11 17:13:02 +0000

    more changes. Put in mainwindow rather than dialog. h/vboxed it. changed notemap approach so it kinda works. Introduced edit existing

--- amenic.py
+++ amenic.py
@@ -9,6 +9,20 @@ import sys
 import vlc
 
 voices = []
+presTable = []
+tWidth = 960
+tHeight = 540
+fRate = 24
+
+# I mean yes, I could define a voicelist class, and then instantiate one instance of it and
+# have it contain the list of voices and give it access methods. But really how would that help?
+
+def getVoiceByName(vName):
+	mess(" there are "+str(len(voices))+" voices stored")
+	for v in voices:
+		if v.vdata["name"] == vName:
+			return v
+	return None
 
 class TableModel(QAbstractTableModel):
 
@@ -42,6 +56,17 @@ class TableModel(QAbstractTableModel):
 			if role == Qt.BackgroundRole:
 				if index.column() == 1 or index.column() == 3:
 					return QColor('#eeeeee')
+				if index.column() == 0 and index.row() == 60 - 21:
+					return QColor('#f01010')
+
+			if role == Qt.DecorationRole:
+				if index.column() == 1:
+					value = self._data[index.row()][index.column()]
+					if value[1] == '#':
+						c = '#000000'
+					else:
+						c = '#ffffff'
+					return QColor(c)
 
 			if role == Qt.DisplayRole or role == Qt.EditRole:
 				val = self._data[index.row()][index.column()]
@@ -58,14 +83,13 @@ class TableModel(QAbstractTableModel):
 		# the length (only works if all rows are an equal length)
 		return len(self._data[0])
 
-
 class voice():
 
 	def __init__(self):
 		self.vdata = {
 			"defImg": None,
 			"restImg": None,
-			"imgTable": [ [ 0,'']],
+			"imgTable": [ ],
 			"name": 'Untitled'
 		}
 
@@ -96,9 +120,17 @@ class vEditD(QDialog):
 		( fname, filter)  = QFileDialog.getOpenFileName(self, 'Open Rest Image', '~/Documents',"Image files (*.png, *.jpg)")
 		self.restEdit.setText(fname)
 
+	def noteRowAdd(self,idx):
+		# This only adds the button to a pre-existing row created by
+		# the SetModel call. It doesn't actually add a row
+		btnGetImg = QPushButton("Select image")
+		btnGetImg.clicked.connect(partial(self.getImg, idx))
+		self.noteMap.setIndexWidget(self.model.index(idx, 2), btnGetImg)
+
 	def initUI(self, v:voice):
 		self.setWindowTitle('Edit Voice')
-		self.resize(400,600)
+		self.resize(800,600)
+		self.setWindowFlags(Qt.CustomizeWindowHint | Qt.FramelessWindowHint | Qt.Dialog | Qt.WindowStaysOnTopHint | Qt.Tool)
 
 		# set up widgets
 		self.nameLabel = QLabel('Name')
@@ -121,26 +153,43 @@ class vEditD(QDialog):
 		self.restLookup.clicked.connect(self.getRestImg)
 
 		self.noteMap = QTableView()
-		#self.model = TableModel()
-		# this row insert routine probably wants extracting to another function
 		# while the actual data in the model comes from vdata["imgTable"],
 		# there's a lot of 'presentation' stuff in the model that we don't want
 		# to store. Conversely, we probably want to retain the bitmap, or a ref
 		# to it in vdata["imgTable"] which wouldn't be presented.
 		presTable = []
-		for idx in range(0, len(v.vdata["imgTable"])):
-			presTable.append([ v.vdata["imgTable"][idx][0], to_name(v.vdata["imgTable"][idx][0]), None, v.vdata["imgTable"][idx][1] ])
+		# v.vdata["imgTable"] is sparse mapped, presTable covers all (practical) possibilities
+		definedNotes ={}
+		if len(v.vdata["imgTable"]) > 0:
+			for iti in range(0,len(v.vdata["imgTable"])):
+				mess("loading into note map "+v.vdata["imgTable"][iti][1])
+				definedNotes[ v.vdata["imgTable"][iti][0] ] = iti
+
+		ci = -1
+		for pti in range (21,108):
+			if pti in definedNotes:
+				idx = definedNotes[pti]
+				presTable.append([ v.vdata["imgTable"][idx][0], to_name(v.vdata["imgTable"][idx][0]), None, v.vdata["imgTable"][idx][1] ])
+				ci = pti
+			else:
+				presTable.append([pti,to_name(pti),None,""])
 
 		self.model = TableModel(presTable)
+		if ci == -1:
+			cidx = self.model.index(60 - 21,0)
+		else:
+			cidx = self.model.index(ci,0)
+		# this is working on a nice-if where we move the ?viewport? to make populated rows visible (if any) or
+		# middle C if not. But I can't figure out how/whether that could be done
 		self.noteMap.setModel(self.model)
-		for idx in range(0, len(v.vdata["imgTable"])):
-			btnGetImg = QPushButton("Select image")
-			btnGetImg.clicked.connect(partial(self.getImg, idx))
-			self.noteMap.setIndexWidget(self.model.index(idx, 2), btnGetImg)
+		for idx in range(0, len(presTable)-1):
+			self.noteRowAdd(idx)
 
-		self.noteMap.setColumnWidth(0,60)
-		self.noteMap.setColumnWidth(1,60)
-		self.noteMap.setColumnWidth(3,180)
+		self.noteMap.verticalHeader().hide()
+
+		self.noteMap.setColumnWidth(0,55)
+		self.noteMap.setColumnWidth(1,80)
+		self.noteMap.setColumnWidth(3,240)
 
 		self.btnCancel = QPushButton('Cancel')
 		self.btnCancel.clicked.connect(self.cancelOut)
@@ -169,10 +218,11 @@ class vEditD(QDialog):
 		vbox.addLayout(hbox3)
 
 		vbox.addWidget(self.noteMap)
-		hbox4 = QHBoxLayout()
-		hbox4.addWidget(self.btnCancel)
-		hbox4.addWidget(self.btnSave)
-		vbox.addLayout(hbox4)
+
+		hbox5 = QHBoxLayout()
+		hbox5.addWidget(self.btnCancel)
+		hbox5.addWidget(self.btnSave)
+		vbox.addLayout(hbox5)
 		self.setLayout(vbox)
 
 	def cancelOut(self):
@@ -186,8 +236,11 @@ class vEditD(QDialog):
 		self.saveV.vdata["defImg"] = self.diEdit.text()
 		self.saveV.vdata["restImg"] = self.restEdit.text()
 		# self.saveV.imgTable -- what exactly?
+		mess("model data size  "+str(len(self.model._data)))
 		for idx in range(0, len(self.model._data)):
-			self.saveV.vdata["imgTable"].append([ self.model._data[idx][0], self.model._data[idx][3]])
+			if self.model._data[idx][3] != None and self.model._data[idx][3] != '':
+				mess("found "+ self.model._data[idx][3]+" in note map")
+				self.saveV.vdata["imgTable"].append([ self.model._data[idx][0], self.model._data[idx][3]])
 		self.accept()
 
 	def getVal(self):
@@ -201,7 +254,8 @@ class vEditD(QDialog):
 		else:
 			self.btnSave.setEnabled(False)
 
-class QAmenicMain(QDialog) :
+#class AmenicMain(QDialog) :
+class AmenicMain(QMainWindow):
 	def __init__(self):
 		super().__init__()
 		icon = QIcon()
@@ -237,6 +291,13 @@ class QAmenicMain(QDialog) :
 
 		droneLabel.setBuddy(self.droneCombo)
 
+		edLabel = QLabel('&Edit Voice',self)
+		self.edCombo = QComboBox(self)
+		self.edCombo.addItem("<Select Voice To Edit>")
+		for v in voices:
+			self.edCombo.addItem(v.name)
+		self.edCombo.currentIndexChanged.connect(self.edSelChg)
+
 		ssLabel = QLabel('&Sound Source',self)
 		self.ssFileShow= QLineEdit(self)
 		self.ssFileShow.readOnly = True
@@ -245,7 +306,7 @@ class QAmenicMain(QDialog) :
 		ssLabel.setBuddy(ssButton)
 
 		theatre = QLabel(self)
-		theatre.setPixmap(QPixmap("/Users/johnhollingum/Documents/AMENIC/Empty.png").scaled(960,540,2,1))
+		theatre.setPixmap(QPixmap("/Users/johnhollingum/Documents/AMENIC/Empty.png").scaled(tWidth,tHeight,2,1))
 
 		self.btnPlay = QPushButton('&Play')
 		self.btnPlay.clicked.connect(self.playsound)
@@ -255,21 +316,38 @@ class QAmenicMain(QDialog) :
 		self.btnStop.clicked.connect(self.stopsound)
 		self.btnStop.setEnabled(False)
 
-		mainLayout = QGridLayout(self)
-		mainLayout.addWidget(dvLabel,0,0)
-		mainLayout.addWidget(self.dvCombo,0,1,1,2)
+		vbox1 = QVBoxLayout()
+		hbox1 = QHBoxLayout()
+		hbox1.addWidget(dvLabel)
+		hbox1.addWidget(self.dvCombo)
 
-		mainLayout.addWidget(droneLabel,1,0)
-		mainLayout.addWidget(self.droneCombo,1,1,1,2)
+		hbox1.addWidget(droneLabel)
+		hbox1.addWidget(self.droneCombo)
 
-		mainLayout.addWidget(ssLabel,2,0)
-		mainLayout.addWidget(self.ssFileShow,2,1)
-		mainLayout.addWidget(ssButton,2,2)
+		hbox1.addWidget(edLabel)
+		hbox1.addWidget(self.edCombo)
 
-		mainLayout.addWidget(theatre,3,1)
+		vbox1.addLayout(hbox1)
 
-		mainLayout.addWidget(self.btnPlay,4,0)
-		mainLayout.addWidget(self.btnStop,4,1)
+		hbox2 = QHBoxLayout()
+		hbox2.addWidget(ssLabel)
+		hbox2.addWidget(self.ssFileShow)
+		hbox2.addWidget(ssButton)
+
+		vbox1.addLayout(hbox2)
+		vbox1.addWidget(theatre)
+
+		hbox3 = QHBoxLayout()
+		hbox3.addWidget(self.btnPlay)
+		hbox3.addWidget(self.btnStop)
+
+		vbox1.addLayout(hbox3)
+
+		container = QWidget()
+		container.setLayout(vbox1)
+
+		# Set the central widget of the Window.
+		self.setCentralWidget(container)
 
 	def dvSelChg(self,i):
 		# this routine responds to an event that it, itself raises
@@ -281,6 +359,7 @@ class QAmenicMain(QDialog) :
 			else:
 				self.droneCombo.addItem(self.playerVn)
 				self.dvCombo.addItem(self.playerVn)
+				self.edCombo.addItem(self.playerVn)
 				i = self.dvCombo.findText(self.playerVn)
 				self.dvCombo.setCurrentIndex(i)
 			return
@@ -298,15 +377,22 @@ class QAmenicMain(QDialog) :
 			else:
 				self.droneCombo.addItem(self.droneVn)
 				self.dvCombo.addItem(self.droneVn)
+				self.edCombo.addItem(self.playerVn)
 				i = self.droneCombo.findText(self.droneVn)
 				self.droneCombo.setCurrentIndex(i)
 				# find droneV from droneVn
 
-		elif self.droneCombo.currentText() != "<none>":
-			mess("use "+self.droneCombo.currentText())
-			# find droneV from droneVn
-		else:
-			self.droneVn = None
+	def edSelChg(self,i):
+		if self.edCombo.currentText() != "<Select Voice To Edit>":
+			mess("edit "+self.edCombo.currentText())
+			edv = getVoiceByName(self.edCombo.currentText())
+			if edv == None:
+				mess("Panic, no stored voice with name '%s'",self.edCombo.currentText())
+			else:
+				edv.edit()
+				mess("edited "+edv.vdata["name"])
+		i = self.edCombo.findText("<Select Voice To Edit>")
+		self.edCombo.setCurrentIndex(i)
 
 	def createNewVoice(self):
 		# in the voice table create a new voice and return its index.
@@ -335,9 +421,8 @@ class QAmenicMain(QDialog) :
 		self.st.stop()
 		self.btnStop.setEnabled(False)
 
-
 def to_name(nn):
-	nnt = [ "C","C#","D","D#","E","F","F#","G","G#","A", "A#","B"]
+	nnt = [ "C  ","C#","D  ","D#","E  ","F  ","F#","G  ","G#","A  ", "A#","B  "]
 	on = int(nn / 12) -1 # opinion seems to differ about whether it's necessary to subtract 1
 	nis = nn % 12
 	nname = nnt[nis]
@@ -366,6 +451,6 @@ def err(mymess):
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	main = QAmenicMain()
+	main = AmenicMain()
 	main.show()
 	sys.exit(app.exec_())
